6122f5fe797640f4f78ea0a81a88046baa1c7a11
carlosvfb <cacavfb2107@gmail.com>
2025-05-12 22:18:41 -0300

    ajuste de detalhes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -151,7 +151,7 @@ export default function Formulario() {
     }
 
     if (verificarDuplicidade()) {
-      setErro("Usuário já cadastrado.");
+      setErro("Já existe uma inscrição com esse nome.");
       return;
     }
 
@@ -409,7 +409,7 @@ export default function Formulario() {
                   }}
                   className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
                 >
-                  Confirmar
+                  Enviar
                 </button>
               </div>
             </div>
